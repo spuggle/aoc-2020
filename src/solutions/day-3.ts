66ce1c51683c5deaab2dfd09771c2f030ad122bb
spuggle <immortalwarrior101@gmail.com>
2020-12-04 02:19:29 +0500

    Day 3 solutions

---
+++ src/solutions/day-3.ts
@@ -0,0 +1,45 @@
+export function partOne(areaMap: AreaMap): number {
+  return areaMap.reduce<number>((treesEncountered, currentRow, rowNumber) => {
+    const columnNumber = (rowNumber * 3) % currentRow.length;
+    const hasEncounteredTree = currentRow[columnNumber] === "#";
+
+    return hasEncounteredTree
+      ? treesEncountered + 1
+      : treesEncountered;
+  }, 0);
+}
+
+export function partTwo(areaMap: AreaMap): number {
+  return ([
+    [ 1, 1 ],
+    [ 3, 1 ],
+    [ 5, 1 ],
+    [ 7, 1 ],
+    [ 1, 2 ]
+  ] as Slope[])
+    .map(slope => treesEncounteredInSlope(slope, areaMap))
+    .reduce<number>((product, treesEncountered) => product * treesEncountered, 1);
+}
+
+function treesEncounteredInSlope([ rightShift, downShift ]: Slope, areaMap: AreaMap): number {
+  let treesEncountered = 0;
+
+  for (let rowNumber = 0; rowNumber < areaMap.length; rowNumber += downShift) {
+    const currentRow = areaMap[rowNumber];
+    const columnNumber = ((rowNumber / downShift) * rightShift) % currentRow.length;
+    const hasEncounteredTree = currentRow[columnNumber] === "#";
+
+    if (hasEncounteredTree) ++treesEncountered;
+  }
+
+  return treesEncountered;
+}
+
+
+export function parse(input: string[]): AreaMap {
+  return input
+    .map(row => row.split(""));
+}
+
+type AreaMap = string[][];
+type Slope = [ number, number ];
