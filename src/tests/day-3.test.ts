79f18e889e06557d9407ac8fb577d8953f8501f5
spuggle <immortalwarrior101@gmail.com>
2020-12-04 02:19:37 +0500

    Day 3 tests

---
+++ src/tests/day-3.test.ts
@@ -0,0 +1,69 @@
+import test from "ava";
+import { parse, partOne, partTwo } from "../solutions/day-3.js";
+
+const singleInput = `
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#`
+  .trim().split("\n");
+
+const repeatedInput = `
+..##.........##.........##.........##.........##.........##.......
+#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+.#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+.#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
+.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+.#........#.#........#.#........#.#........#.#........#.#........#
+#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+#...##....##...##....##...##....##...##....##...##....##...##....#
+.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#`
+  .trim().split("\n");
+
+const parsedSingleInput = singleInput.map(row => row.split(""));
+const parsedRepeatedInput = repeatedInput.map(row => row.split(""));
+
+test("Parse the repeated input", tester => {
+  const parseOutput = parse(repeatedInput);
+
+  tester.deepEqual(parseOutput, parsedRepeatedInput);
+});
+
+test("Parse the single input", tester => {
+  const parseOutput = parse(singleInput);
+
+  tester.deepEqual(parseOutput, parsedSingleInput);
+});
+
+test("[Part 1] Find the correct number of trees encountered with repeated input", tester => {
+  const solution = partOne(parsedRepeatedInput);
+
+  tester.is(solution, 7);
+});
+
+test("[Part 1] Find the correct number of trees encountered with non-repeated input", tester => {
+  const solution = partOne(parsedSingleInput);
+
+  tester.is(solution, 7);
+});
+
+test("[Part 2] Find the correct product of number of trees encountered with repeated input", tester => {
+  const solution = partTwo(parsedRepeatedInput);
+
+  tester.is(solution, 336);
+});
+
+test("[Part 2] Find the correct product of number of trees encountered with non-repeated input", tester => {
+  const solution = partTwo(parsedSingleInput);
+
+  tester.is(solution, 336);
+});
